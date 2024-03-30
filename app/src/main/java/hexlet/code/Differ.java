347342ce3980e3594737e78ac85f4eca22ff5530
Eugene <braincucumber@mail.ru>
2024-03-30 16:29:32 +0300

    differ fix

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -12,4 +12,11 @@ public class Differ {
         var parsedMap = Parser.parse(firstPath, secondPath);
         return Formatter.format(parsedMap, format);
     }
+
+    public static String generate(String filePath1, String filePath2) throws IOException {
+        Path firstPath = Paths.get(filePath1).toAbsolutePath();
+        Path secondPath = Paths.get(filePath2).toAbsolutePath();
+        var parsedMap = Parser.parse(firstPath, secondPath);
+        return Formatter.format(parsedMap, "stylish");
+    }
 }
